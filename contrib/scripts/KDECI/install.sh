2e1663db627db3d1eabd70e278817587a2762a32
Marko Käning <mk-lists@email.de>
2014-05-27 08:06:02 +0200

    KDECI: helper script for running prepare and build scripts at once

---
+++ contrib/scripts/KDECI/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUILD_DIR=/Users/marko/WC/KDECI-builds
+
+if [ "x$1" != "x" ]; then
+	BDIR=${BUILD_DIR}/$1
+
+	. prepare.sh $1 && (. build.sh $1)
+else
+        echo "Usage: $0 PROJECT_NAME"
+fi
