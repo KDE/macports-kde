cc3da3575ef593ce09002189a87c7c94486e1a68
Marko Käning <mk-lists@email.de>
2014-06-15 22:48:48 +0200

    KDE/CI: find git commits in prepare.sh

--- contrib/scripts/KDECI/prepare.sh
+++ contrib/scripts/KDECI/prepare.sh
@@ -4,12 +4,17 @@ BUILD_DIR=/Users/marko/WC/KDECI-builds
 
 if [ "x$1" != "x" ]; then
 	BDIR=${BUILD_DIR}/$1
+	LOG=${BDIR}/KDECI-build.log
 
 	[ ! -d ${BDIR} ] && (echo "Creating still missing build directory..."; mkdir $BDIR )
+	[ ! -f ${LOG} ] && rm ${LOG}
 
 	(cd ~/scripts; \
 		python2.7 tools/prepare-environment.py --project $1 --branchGroup kf5-qt5 \
-		--platform darwin-mavericks --sources ${BDIR} )
+		--platform darwin-mavericks --sources ${BDIR} > ${LOG})
+
+	grep "Receiving objects:" ${LOG} > /dev/null
+
 	exit $?
 else
 	echo "Usage: $0 PROJECT_NAME"
