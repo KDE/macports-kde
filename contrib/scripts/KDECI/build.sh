aa67a25395d2e181d1962ef948627d9d6cc3367d
Marko Käning <mk-lists@email.de>
2014-06-28 21:07:21 +0200

    KDE/CI: fix bug in build.sh and re-enable copying of kdoctools' files

--- contrib/scripts/KDECI/build.sh
+++ contrib/scripts/KDECI/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+declare -i RETURN_VALUE
+
 BUILD_DIR=/Users/marko/WC/KDECI-builds
 
 if [ "x$1" != "x" ]; then
@@ -14,16 +16,17 @@ if [ "x$1" != "x" ]; then
 		python2.7 tools/perform-build.py --project $1 --branchGroup kf5-qt5 \
 		--platform darwin-mavericks --sources ${BDIR} ) &> ${LOG}
 
-	if [ $? ]; then
+	RETURN_VALUE=$?
+	if [ ! $RETURN_VALUE ]; then
 		if [ "$1" == "kdoctools" ]; then
-			echo "Copying kdoctools' files to /Library/Application Support/ doesn't seem to be necessary anymore!"
-#			echo "Copying kdoctools' files to /Library/Application Support/ ..."
-#			rm -rf /Library/Application\ Support/kf5/kdoctools/
-#			cp -Rp /opt/kde/install/darwin/mavericks/clang/kf5-qt5/frameworks/kdoctools/inst/Library/Application\ Support/kf5 /Library/Application\ Support
+#			echo "Copying kdoctools' files to /Library/Application Support/ doesn't seem to be necessary anymore!"
+			echo "Copying kdoctools' files to /Library/Application Support/ ..."
+			rm -rf /Library/Application\ Support/kf5/kdoctools/
+			cp -Rp /opt/kde/install/darwin/mavericks/clang/kf5-qt5/frameworks/kdoctools/inst/Library/Application\ Support/kf5 /Library/Application\ Support
 		fi
 	fi
 	echo "=========================================="
-	exit $?
+	exit $RETURN_VALUE
 else
 	echo "Usage: $0 PROJECT_NAME"
 	exit -1
