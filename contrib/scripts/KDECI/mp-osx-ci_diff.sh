4cbee816b9393475da032045c057e54ffb12bee0
Marko Käning <mk-lists@email.de>
2014-07-20 13:21:55 +0200

    KDE/CI: introduce a helper script which supports keeping the CI system config synced with the git repo

---
+++ contrib/scripts/KDECI/mp-osx-ci_diff.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# This assumes the existence of branch mp-osx-ci branched off from production branch.
+#
+# The diff to current production will be put into the corresponding patch file in the MacPorts/KDE git clone.
+#
+# Only the included files/directories are considered.
+#
+
+INCLUDES="config tools"
+
+git checkout mp-osx-ci
+git diff production $INCLUDES >~/WC/GIT/macports-kde/contrib/scripts/KDECI/patch-config-build-darwin-mavericks.cfg.diff
