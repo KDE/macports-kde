5b3d468a91a601f0c5f2d3d1813d0469822cfab5
Marko Käning <mk-lists@mailbox.org>
2015-06-30 00:20:32 +0200

    KF5: check out KBM repo

--- contrib/scripts/KF5/update-setup.sh
+++ contrib/scripts/KF5/update-setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Checkout mp-osx-ci
 if [ ! -d mp-osx-ci ]; then
         mkdir -p mp-osx-ci
@@ -9,5 +11,19 @@ pushd mp-osx-ci
     fi
     git fetch origin
     git checkout mp-osx-ci
+)
+popd
+
+# Checkout kde-build-metadata
+if [ ! -d kde-build-metadata ]; then
+        mkdir -p kde-build-metadata
+fi
+pushd kde-build-metadata
+(
+    if [ ! -d .git ]; then
+        git clone git://anongit.kde.org/kde-build-metadata.git .
+    fi
+    git fetch origin
+    git checkout master
 )
 popd
